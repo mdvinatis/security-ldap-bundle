31cea2fef6d06016607c96fc980de59dcb5624a3
Michel Dourneau <mdourneau@vinatis.com>
2022-07-01 06:37:50 +0200

    fix VinatisSecurityLdapExtension

--- src/DependencyInjection/VinatisSecurityLdapExtension.php
+++ src/DependencyInjection/VinatisSecurityLdapExtension.php
@@ -33,7 +33,7 @@ final class VinatisSecurityLdapExtension extends Extension
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new YamlFileLoader($container, new FileLocator(dirname(__DIR__) . '/Resources/config'));
-        $loader->load('commands.yaml');
+        $loader->load('services.yaml');
 
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
